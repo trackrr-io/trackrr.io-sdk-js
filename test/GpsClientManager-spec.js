398478e197d1a3b7b947aa6a06082c85c1c2a928
hasegawa <hasegawa@hasegawa4>
2016-07-25 18:59:09 +0900

    削除

--- test/GpsClientManager-spec.js
+++ test/GpsClientManager-spec.js
@@ -1,5 +1,5 @@
 var GpsClientManager = require("../lib/GpsClientManager");
-var gpsKey = "844ebb8f1c9deb4bcbaf1acf765c90d34c19fde2bd6b590556015e56f7ba323647c520f2e409d4f9f93f";
+var gpsKey = "xxxxxxxxxxxxx";
 var secret = "";
 
 describe("greeter", function () {
